eadbb267fb153e64d6ee88d3e21e436a2a0d10e6
Arvin Perucho <perucar@MFCGD.COM>
2022-08-26 04:28:26 -0400

    initia

---
+++ Server/utils/db_connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+const con = mysql.createPool({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "skpdb",
+});
+
+const getConnection = function (callback) {
+  con.getConnection(function (err, conn) {
+    if (err) {
+      return callback(err);
+    }
+    console.log("connected to database");
+    callback(err, conn);
+  });
+};
+
+module.exports = getConnection;
